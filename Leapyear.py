ce6184f16fd96e86c3db5dbe36f431b0e30fece4
Praveenrajsoundar <144544710+Praveenrajsoundar@users.noreply.github.com>
2023-09-09 21:44:05 +0530

    Nan mudalvan challenge unit 1
    Fundamentals of clouding and computing

---
+++ Leapyear.py
@@ -0,0 +1,15 @@
+`
+# Get the year input from the user
+year = int(input("Enter a year: "))
+
+# Check if it's a leap year
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")
+```￼Notainly! Here's a Python program that determines whether a year entered by the user is a leap year or not using if-elif-else statements:
+
+```python
+# Get the year input from the user
+year = int(input("Enter a year: "))
+
